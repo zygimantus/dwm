4f2ae09a3855529f9a25044f3127e85640f1e4f7
zygimantus <zygimantus@gmail.com>
2022-03-13 08:02:08 +0200

    Fixing typo

--- config.def.h
+++ config.def.h
@@ -65,7 +65,7 @@ static Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY		        XK_w, 	   spawn,          {.v = web } },
+	{ MODKEY,		        XK_w, 	   spawn,          {.v = web } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
 	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
 	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
